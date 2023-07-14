7694e2caa4f62a4436be2ea351f6dd35d119cd8d
consorcioCAP1 <omar.dcruz12@gmail.com>
2023-07-14 10:47:46 -0500

    first commit

---
+++ src/main/java/com/nttdata/bootcamp/customerbankaccountservice/CustomerBankAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.customerbankaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerBankAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerBankAccountServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/bootcamp/customerbankaccountservice/CustomerBankAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.customerbankaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerBankAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
